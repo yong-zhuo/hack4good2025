93107adee0474ccaca1619bd9c525697e7341053
hweinian <hweinian@gmail.com>
2025-01-13 22:06:17 +0800

    Add order dashboard

--- components/navbar/Logo.jsx
+++ components/navbar/Logo.jsx
@@ -1,10 +1,11 @@
 'use client'
 
+import Link from 'next/link'
 import React from 'react'
 
 const Logo = () => {
   return (
-    <div className='items-center font-sans font-extrabold text-xl text-[#FBF5E5]'>Marketplace</div>
+    <div className='items-center font-sans font-extrabold text-xl text-[#FBF5E5]'><Link href="/home" className='hover:underline text-[#FBF5E5]'>Marketplace</Link></div>
   )
 }
 
